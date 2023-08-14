c6315fb2c45bf6d880e36afbe03ae7396c7bfb1c
felipetop <felipejorgetop@gmail.com>
2023-08-14 08:51:49 -0300

    feat: add jsdoc rules

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,20 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+/**
+ * @fileoverview Configuração do ambiente de teste do Angular utilizando o Karma.
+ * Localiza e executa todos os arquivos de teste com a extensão .spec.ts.
+ */
 
 import 'zone.js/testing';
+
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
+/**
+ * Declaração da função `require`, usada para carregar arquivos de teste.
+ * @typedef {{context: Function}} require
+ */
 declare const require: {
   context(path: string, deep?: boolean, filter?: RegExp): {
     <T>(id: string): T;
@@ -14,13 +22,16 @@ declare const require: {
   };
 };
 
-// First, initialize the Angular testing environment.
+// Inicializa o ambiente de teste do Angular.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
 );
 
-// Then we find all the tests.
+/**
+ * Localiza todos os arquivos de teste com a extensão .spec.ts.
+ * @type {{keys: Function, <T>(id: string): T}}
+ */
 const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
+
 context.keys().map(context);

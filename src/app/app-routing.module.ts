191b04543c285c626d024bbca1c6bc2c0711aefb
felipetop <felipejorgetop@gmail.com>
2023-08-15 20:07:59 -0300

    feat: add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,44 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { ItemListModule } from './item-list/item-list.module';
+import { SinglePageModule } from './single-page/single-page.module';
 
-const routes: Routes = [];
+type RouteConfig = {
+  [key: string]: string;
+};
+
+const routeConfig: RouteConfig = {
+  itemList: 'item-list',
+  singlePage: 'single-page'
+};
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: routeConfig['itemList'],
+        loadChildren: (): Promise<ItemListModule> =>
+          import('./item-list/item-list.module').then(m => m.ItemListModule),
+      },
+      {
+        path: routeConfig['singlePage'],
+        loadChildren: (): Promise<SinglePageModule> =>
+          import('./single-page/single-page.module').then(m => m.SinglePageModule),
+      },
+    ]
+  },
+];
+
+const routerOptions: ExtraOptions = {
+  useHash: false,
+  anchorScrolling: 'enabled',
+  onSameUrlNavigation: 'reload',
+  scrollPositionRestoration: 'enabled'
+};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, routerOptions)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }

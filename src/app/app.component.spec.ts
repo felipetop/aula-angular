693009f420a827ae3dce99528dfed5fbe2d3491b
felipetop <felipejorgetop@gmail.com>
2023-08-18 07:33:16 -0300

    fix: unit tests after migration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { PokemonService } from './service/pokemon-data.service';
 
 describe('AppComponent', () => {
+  let pokemonServiceMock: Partial<PokemonService>;
+
   beforeEach(async () => {
+    pokemonServiceMock = {
+      getSpecies: jasmine.createSpy('getSpecies')
+    };
+
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
@@ -11,6 +18,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: PokemonService, useValue: pokemonServiceMock } // Use o mock no lugar do serviço real
+      ]
     }).compileComponents();
   });
 
@@ -18,6 +28,12 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should call getSpecies from PokemonService on initialization', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges(); // Chama ngOnInit
+    expect(pokemonServiceMock.getSpecies).toHaveBeenCalled();
   });
 
 });
